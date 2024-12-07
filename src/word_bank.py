f3dca2dea9dfeb4f4ef5c7d8332f0a7d445f0daf
Shanqi Sun <shanqisun@Shanqis-MBP.attlocal.net>
2024-12-06 20:35:13 -0600

    feat(GFP-001): add game bank

---
+++ src/word_bank.py
@@ -0,0 +1,6 @@
+# word_bank.py
+import random
+
+def get_random_word():
+    words = ['python', 'developer', 'programming', 'algorithm', 'debug', 'variable', 'function', 'loop']
+    return random.choice(words)
